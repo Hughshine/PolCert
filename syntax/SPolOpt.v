499c474a6524ca4f900aa8b8946193817a69f8ff
Xuyang Li <li5274@purdue.edu>
2026-03-07 00:12:05 +0300

    syntax: route polopt through prepared pipeline

--- syntax/SPolOpt.v
+++ syntax/SPolOpt.v
@@ -4,29 +4,22 @@ Require Import Linalg.
 Require Import OpenScop.
 Require Import Result.
 Require Import PolOpt.
+Require Import PolOptPrepared.
+Require Import PrepareCodegen.
 Require Import ImpureAlarmConfig.
 Require Import Vpl.Impure.
 
 Module CoreOpt := PolOpt SPolIRs.
+Module PreparedOpt := PolOptPrepared SPolIRs.
+Module Prepare := PrepareCodegen SPolIRs.
 Module SVal := CoreOpt.Validator.
 
-Definition codegen (pol : SPolIRs.PolyLang.t) : imp SPolIRs.Loop.t :=
-  let '(pis, varctxt, vars) := pol in
-  let n :=
-    fold_right Nat.max 0
-      (map (fun pi => Linalg.poly_nrl pi.(SPolIRs.PolyLang.pi_poly)) pis) in
-  BIND loop <- CoreOpt.CodeGen.complete_generate_many (length varctxt) n pis -;
-  pure (loop, varctxt, vars).
-
 Definition opt : SPolIRs.Loop.t -> imp SPolIRs.Loop.t :=
-  fun loop =>
-    BIND pol <- res_to_alarm SPolIRs.PolyLang.dummy (CoreOpt.Extractor.extractor loop) -;
-    BIND pol' <- CoreOpt.scheduler' pol -;
-    codegen pol'.
+  PreparedOpt.Opt.
 
 Definition opt_poly (pol : SPolIRs.PolyLang.t) : imp SPolIRs.Loop.t :=
   BIND pol' <- CoreOpt.scheduler' pol -;
-  codegen pol'.
+  Prepare.prepared_codegen pol'.
 
 Definition opt_scop (scop : OpenScop) : imp SPolIRs.Loop.t :=
   match SPolIRs.PolyLang.from_openscop_complete scop with
